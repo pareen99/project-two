4a030e84e2b024b3c8dace0ff15d4e0d68f33a95
Pareen Bhagat <pareenbhagat@Pareens-Air.attlocal.net>
2020-10-26 10:43:06 -0400

    js

--- js/script.js
+++ js/script.js
@@ -20,3 +20,41 @@ menu.addEventListener("click", toggleMenu);
 //execute toggle function from overlay on click
 overlay.addEventListener("click", toggleMenu);
 /*Navbar*/
+
+/*Accordion*/
+
+//get the accordion element 
+var accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i=0; i<accordion.length; i++) {
+    acc[i].addEventListener ("click", function() {
+     /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle ("active");   
+
+     /* Toggle between hiding and showing the active panel */
+     var panel = this.nextElementSibling;
+     if (panel.style.display === "block") {
+         panel.style.display = "none";
+     } else {
+         panel.style.display = "block";
+     }
+    });
+
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i=0; i< acc.length;i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
