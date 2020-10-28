5155f2c9a3d217e1ba0aa4bad4ce6346339e4e7b
Pareen Bhagat <pareenbhagat@Pareens-Air.attlocal.net>
2020-10-27 20:55:05 -0400

    changes

--- js/script.js
+++ js/script.js
@@ -28,10 +28,10 @@ var accordion = document.getElementsByClassName("accordion");
 var i;
 
 for (i=0; i<accordion.length; i++) {
-    acc[i].addEventListener ("click", function() {
+    accordion[i].addEventListener("click", function() {
      /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    this.classList.toggle ("active");   
+    this.classList.toggle("active");   
 
      /* Toggle between hiding and showing the active panel */
      var panel = this.nextElementSibling;
